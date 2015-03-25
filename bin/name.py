2cbaa4bb26880dce2bb17fceb8f9302565a2e0a0
Jamie Oaks <joaks1@gmail.com>
2015-03-25 12:25:30 -0700

    Adding script for random call.

---
+++ bin/name.py
@@ -0,0 +1,77 @@
+#!/usr/bin/env python
+
+"""
+Draw names at random.
+"""
+
+import os
+import sys
+import random
+import datetime
+import argparse
+
+from pymsbayes.utils import argparse_utils
+
+participation_dir = '../.participation'
+
+participation_path = os.path.join(participation_dir, 'participation.log')
+
+_program_info = {
+    'name': os.path.basename(__file__),
+    'author': 'Jamie Oaks',
+    'version': 'Version 0.1.0',
+    'description': __doc__,
+    'copyright': 'Copyright (C) 2014 Jamie Oaks',
+    'license': 'GNU GPL version 3 or later',}
+
+def get_random_line(file_path):
+    with open(file_path, 'rU') as file_stream:
+        current_line = next(file_stream)
+        for i, line in enumerate(file_stream):
+            if random.randrange(i + 2):
+                continue
+            current_line = line
+    return current_line
+
+def main():
+    section_options = ['a', 'b']
+    description = '{name} {version}'.format(**_program_info)
+    parser = argparse.ArgumentParser(description = description)
+    parser.add_argument('--version',
+            action='version',
+            version='%(prog)s ' + _program_info['version'],
+            help='report version and exit')
+    parser.add_argument('section',
+            type = str,
+            choices=section_options,
+            help = ('The class section.'))
+    args = parser.parse_args()
+
+    name = get_random_line('../class-lists/{0}-names.txt'.format(args.section)).strip()
+
+    sys.stdout.write('\n{0}\n\n'.format(name))
+    response = raw_input('(y)es/(n)o/(c)ancel: ')
+    r = response.strip()[0].lower()
+    participated = 0
+    if r == 'y':
+        participated = 1
+    elif r == 'n':
+        participated = 0
+    else:
+        sys.exit(0)
+    
+
+    if not os.path.isdir(participation_dir):
+        os.mkdir(participation_dir)
+
+    if not os.path.isfile(participation_path):
+        with open(participation_path, 'w') as out:
+            out.write('name\tsection\tparticipated\ttime\n')
+
+    with open(participation_path, 'a') as out:
+        out.write('{0}\t{1}\t{2}\t{3}\n'.format(name, args.section,
+                participated, str(datetime.datetime.now())))
+
+if __name__ == '__main__':
+    main()
+
