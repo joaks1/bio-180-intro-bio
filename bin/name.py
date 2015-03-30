83834518fe5bdb702432fa34d1e3c489cf7139f4
Jamie Oaks <joaks1@gmail.com>
2015-03-29 17:21:25 -0700

    Updating random call list script.

--- bin/name.py
+++ bin/name.py
@@ -41,36 +41,50 @@ def main():
             action='version',
             version='%(prog)s ' + _program_info['version'],
             help='report version and exit')
-    parser.add_argument('section',
+    parser.add_argument('-s', '--section',
             type = str,
             choices=section_options,
             help = ('The class section.'))
+    parser.add_argument('-n', '--number',
+            type = int,
+            default = 1,
+            help = ('The number of names to draw.'))
     args = parser.parse_args()
 
-    name = get_random_line('../class-lists/{0}-names.txt'.format(args.section)).strip()
+    name_path = '../class-lists/names.txt'
+    if args.section:
+        '../class-lists/{0}-names.txt'.format(args.section)
 
-    sys.stdout.write('\n{0}\n\n'.format(name))
-    response = raw_input('(y)es/(n)o/(c)ancel: ')
-    r = response.strip()[0].lower()
-    participated = 0
-    if r == 'y':
-        participated = 1
-    elif r == 'n':
+    if args.number < 2:
+        name = get_random_line(name_path).strip()
+
+        sys.stdout.write('\n{0}\n\n'.format(name))
+        response = raw_input('(y)es/(n)o/(c)ancel: ')
+        r = response.strip()[0].lower()
         participated = 0
-    else:
+        if r == 'y':
+            participated = 1
+        elif r == 'n':
+            participated = 0
+        else:
+            sys.exit(0)
+        
+
+        if not os.path.isdir(participation_dir):
+            os.mkdir(participation_dir)
+
+        if not os.path.isfile(participation_path):
+            with open(participation_path, 'w') as out:
+                out.write('name\tsection\tparticipated\ttime\n')
+
+        with open(participation_path, 'a') as out:
+            out.write('{0}\t{1}\t{2}\t{3}\n'.format(name, args.section,
+                    participated, str(datetime.datetime.now())))
         sys.exit(0)
-    
 
-    if not os.path.isdir(participation_dir):
-        os.mkdir(participation_dir)
-
-    if not os.path.isfile(participation_path):
-        with open(participation_path, 'w') as out:
-            out.write('name\tsection\tparticipated\ttime\n')
-
-    with open(participation_path, 'a') as out:
-        out.write('{0}\t{1}\t{2}\t{3}\n'.format(name, args.section,
-                participated, str(datetime.datetime.now())))
+    for i in range(args.number):
+        name = get_random_line(name_path).strip()
+        sys.stdout.write('{0}\n'.format(name))
 
 if __name__ == '__main__':
     main()
